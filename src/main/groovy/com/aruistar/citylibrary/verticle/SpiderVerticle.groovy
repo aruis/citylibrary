91839fb9f622982ea4051039e2ab4d581d0d3c30
lzx <307871680@qq.com>
2018-07-08 21:48:58 +0800

    防止超时，提高稳定性

--- src/main/groovy/com/aruistar/citylibrary/verticle/SpiderVerticle.groovy
+++ src/main/groovy/com/aruistar/citylibrary/verticle/SpiderVerticle.groovy
@@ -35,52 +35,57 @@ class SpiderVerticle extends AbstractVerticle {
         def url = "http://www.creprice.cn/city/"
         // 获取creprice城市列表
         WebClient webClientCity = WebClient.create(vertx, new WebClientOptions().setTryUseCompression(true).setMaxPoolSize(10))
-        jsonCity.getJsonArray("cities").each { JsonObject city ->
-            webClientCity.getAbs("${url + city.getString("cityCode")}.html")
-                    .timeout(10000)
-                    .send({
-                if (it.succeeded()) {
-                    def html = it.result().bodyAsString()
-                    def doc = Jsoup.parse(html)
-                    def cityName = doc.getElementsByClass("citySelect_header")[0].text()
+        jsonCity.getJsonArray("cities").eachWithIndex { JsonObject city, int cityIndex ->
+            // 防止超时，等待1s再请求，351个城市351s在可接受范围
+            vertx.setTimer(1000 * cityIndex + 1, {
+                webClientCity.getAbs("${url + city.getString("cityCode")}.html")
+                        .timeout(10000)
+                        .send({
+                    if (it.succeeded()) {
+                        def html = it.result().bodyAsString()
+                        def doc = Jsoup.parse(html)
+                        def cityName = doc.getElementsByClass("citySelect_header")[0].text()
 
-                    def elements = doc.getElementsByClass("u_area")
-                    def cityPrice = [:]
-                    cityPrice.city = cityName
-                    cityPrice.price = []
-                    elements.eachWithIndex { div, index ->
-                        // class tith 的标题
-                        def tiths = div.getElementsByClass("bt")
+                        def elements = doc.getElementsByClass("u_area")
+                        def cityPrice = [:]
+                        cityPrice.city = cityName
+                        cityPrice.price = []
+                        elements.eachWithIndex { div, index ->
+                            // class tith 的标题
+                            def tiths = div.getElementsByClass("bt")
 
-                        def u_lists = div.getElementsByClass("u_list")
-                        // 上个月，这个月，今日和未来的价格
-                        u_lists.eachWithIndex { u_list, int i ->
-                            def lis = u_list.getElementsByTag("li")
-                            lis.each { li ->
-                                def children = li.childNodes()
-                                def text = tiths[i].text()
-                                children.each { child ->
-                                    text += child.text()
+                            def u_lists = div.getElementsByClass("u_list")
+                            // 上个月，这个月，今日和未来的价格
+                            u_lists.eachWithIndex { u_list, int i ->
+                                def lis = u_list.getElementsByTag("li")
+                                lis.each { li ->
+                                    def children = li.childNodes()
+                                    def text = tiths[i].text()
+                                    children.each { child ->
+                                        text += child.text()
+                                    }
+                                    /**
+                                     * 上月9,331 元/㎡ 同比 +26.03%
+                                     * 上月平均总价： 109万元
+                                     * 上月售租比： 42年
+                                     * 上月挂牌市值： 176.8亿元
+                                     * 上月挂牌数量： 1.6万套
+                                     * 近一月9,398 元/㎡ 环比 +0.72%
+                                     * 近一月新增房源 16,356 套
+                                     * 今日9,628 元/㎡ 环比 +2.44%
+                                     * 预测未来一月 9,520 元/㎡
+                                     *
+                                     */
+                                    cityPrice.price << text
                                 }
-                                /**
-                                 * 上月9,331 元/㎡ 同比 +26.03%
-                                 * 上月平均总价： 109万元
-                                 * 上月售租比： 42年
-                                 * 上月挂牌市值： 176.8亿元
-                                 * 上月挂牌数量： 1.6万套
-                                 * 近一月9,398 元/㎡ 环比 +0.72%
-                                 * 近一月新增房源 16,356 套
-                                 * 今日9,628 元/㎡ 环比 +2.44%
-                                 * 预测未来一月 9,520 元/㎡
-                                 *
-                                 */
-                                cityPrice.price << text
                             }
                         }
+                        // todo save to database
+                        println(cityPrice)
+                    } else {
+                        println(it.cause().message)
                     }
-                    // todo save to database
-                    println(cityPrice)
-                }
+                })
             })
         }
     }
